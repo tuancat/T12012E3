702ec93a6278bad88b99c1bca410e418dd336d35
tuan.catnghiemhieu@gmail.com <tuan.catnghiemhieu@gmail.com>
2022-09-10 19:08:34 +0700

    - update

--- lab4/src/main/java/com/sam/lab4/controller/AddController.java
+++ lab4/src/main/java/com/sam/lab4/controller/AddController.java
@@ -1,4 +1,10 @@
 package com.sam.lab4.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/add")
 public class AddController {
 }
